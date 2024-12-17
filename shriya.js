0e34ed09469f3113dd50df0e3b8a80af5b04dc3d
HACKERPLAYZ41 <utkarshhalwai9@gmail.com>
2024-12-17 21:20:05 +0530

    Add files via upload

---
+++ shriya.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("Page Loaded!");
+
+    // Example JavaScript: Show an alert with a message
+    setTimeout(() => {
+        alert("Hey, I hope you like this page! 😊");
+    }, 2000);
+});
