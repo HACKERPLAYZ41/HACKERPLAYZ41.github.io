8bc65bfff527347789712b28ae97f423bac431de
HACKERPLAYZ41 <utkarshhalwai9@gmail.com>
2026-08-25 03:25:57 +0530

    Remove nested zip backup folder from git tracking

--- HACKERPLAYZ41.github.io-main/HACKERPLAYZ41.github.io-main/shriya.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    console.log("Page Loaded!");
-
-    // Example JavaScript: Show an alert with a message
-    setTimeout(() => {
-        alert("Hey, I hope you like this page! 😊");
-    }, 2000);
-});
