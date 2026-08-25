80ead9652a6d0e47bafdb088730a9cc2686ac861
HACKERPLAYZ41 <utkarshhalwai9@gmail.com>
2026-08-25 18:09:06 +0530

    chore: clean up unneeded legacy root html/js files and keep root repository clean

--- login page/login.js
+++
@@ -1,9 +0,0 @@
-const input = document.getElementById('password');
-const toggle = document.getElementById("togglePassword");
-toggle.addEventListener('click', () => {
-    const ispassword = ispassword = input.type === 'password';
-    input.type = ispassword ? 'text' : 'password';
-    toggle.className = ispassword
-     ? "ri-eye-off-line togglePassword"
-     : "ri-eye-line togglePassword";
-});
